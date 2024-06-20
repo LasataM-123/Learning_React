4392f1a029aa68a28f40911b6b31411d9f10bc2a
Lasata <lasatamaharjan2006@gmail.com>
2024-06-20 21:17:11 +0545

    Today's work routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-// import logo from "./logo.svg";
-import { useState } from "react";
 import "./App.css";
-// import Hello from "./Hello";
-// import Form from "./Form";
-// import Setstate from "./Setstate";
-// import Calculator from "./Calculator";
+import Form from "./Form";
+import Calculator from "./Calculator";
 import Todo from "./Todo";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      {/* <Form /> */}
-      {/* <Setstate /> */}
-      {/* <Calculator /> */}
-      <Todo />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
