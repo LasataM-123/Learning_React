f87a152514e6981f3c599c58df102c05a11d65ab
Lasata <lasatamaharjan2006@gmail.com>
2024-06-21 14:14:07 +0545

    Color change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import "./App.css";
-import Form from "./Form";
-import Calculator from "./Calculator";
-import Todo from "./Todo";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Form from "./Form";
+// import Calculator from "./Calculator";
+// import Todo from "./Todo";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Color from "./Color";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/calculator">Calculator</Link>
-            </li>
-            <li>
-              <Link to="/form">Form</Link>
-            </li>
-            <li>
-              <Link to="/todo">Todo</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/todo" element={<Todo />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Color />
+    </div>
   );
 }
 
 export default App;
+{
+  /* <BrowserRouter>
+<div>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/form">Form</Link>
+      </li>
+      <li>
+        <Link to="/todo">Todo</Link>
+      </li>
+    </ul>
+  </nav>
+
+  <Routes>
+    <Route path="/calculator" element={<Calculator />} />
+    <Route path="/form" element={<Form />} />
+    <Route path="/todo" element={<Todo />} />
+  </Routes>
+</div>
+</BrowserRouter> */
+}
---
+++ src/Color.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./Color.css";
+const Color = () => {
+  const [bgcolor, setBgcolor] = useState("");
+  const handleColourChange = () => {
+    const colors = [
+      "red",
+      "blue",
+      "black",
+      "white",
+      "pink",
+      "purple",
+      "brown",
+      "green",
+      "yellow",
+      "orange",
+    ];
+    const random = colors[Math.floor(Math.random() * colors.length)];
+    setBgcolor(random);
+  };
+  const handleRgbChange = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const random = "rgb(" + red + "," + green + "," + blue + ")";
+    // const random=`rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    setBgcolor(random);
+  };
+  const handleHexChange = () => {
+    const random =
+      "#" +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, "0");
+    setBgcolor(random);
+  };
+  useEffect(() => {
+    console.log("Background color changed to: " + bgcolor);
+  }, [bgcolor]);
+  return (
+    <div>
+      <div
+        className="color-container"
+        style={{
+          backgroundColor: bgcolor,
+        }}
+      >
+        <button onClick={handleColourChange}>Change Color</button>
+        <button onClick={handleRgbChange}>Change RGB</button>
+        <button onClick={handleHexChange}>Change HEX</button>
+        <h1>Background color changed to: {bgcolor}</h1>
+      </div>
+    </div>
+  );
+};
+export default Color;
