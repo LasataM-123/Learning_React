6ac5b30b92d0f9fa832159da30294ed0fdb9d8cc
Lasata <lasatamaharjan2006@gmail.com>
2024-07-16 16:04:13 +0545

    Completed material UI

--- src/MaterialUI.jsx
+++ src/MaterialUI.jsx
@@ -6,36 +6,41 @@ import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import Rating from "@mui/material/Rating";
 import Switch from "@mui/material/Switch";
-
+import Phewa from "./assets/Phewa.jpg";
+import Chandragiri from "./assets/Chandragiri.jpg";
+import Rara from "./assets/Rara.jpg";
+import Machapuchare from "./assets/Machapuchare.jpg";
+import Chitwan from "./assets/Chitwan.jpg";
+import Annapurna from "./assets/Annapurna.jpg";
 export const MaterialUI = () => {
   const places = [
     {
-      img: "./assets/Phewa.jpg",
+      img: Phewa,
       name: "Phewa Lake",
       typo: "Phewa Lake, Pokhara, Nepal",
     },
     {
-      img: "./assets/Chandragiri.jpg",
+      img: Chandragiri,
       name: "Chandragiri Hills",
       typo: "Chandragiri Hills, Kathmandu, Nepal",
     },
     {
-      img: "./assets/Rara.jpg",
+      img: Rara,
       name: "Rara Lake",
       typo: "Rara Lake, Mugu, Nepal",
     },
     {
-      img: "./assets/Machapuchare.jpg",
+      img: Machapuchare,
       name: "Machapuchare",
       typo: "Machapuchare Mountain, Pokhara, Nepal",
     },
     {
-      img: "./assets/Chitwan.jpg",
+      img: Chitwan,
       name: "Chitwan National Park",
       typo: "Chitwan National Park, Chitwan, Nepal",
     },
     {
-      img: "./assets/Annapurna.jpg",
+      img: Annapurna,
       name: "Annapurna Base Camp",
       typo: "Annapurna Base Camp, Pokhara, Nepal",
     },
@@ -52,10 +57,45 @@ export const MaterialUI = () => {
 
   return (
     <div className="famous-places">
-      <div className="places">
+      <h1 style={{ textAlign: "center" }}>
+        <u>FAMOUS PLACES OF NEPAL</u>
+      </h1>
+      <div
+        className="places"
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "40px",
+        }}
+      >
         {places.map((items, index) => (
-          <Card key={index}>
-            <CardMedia component="img" alt={items.name} image={items.img} />
+          <Card
+            key={index}
+            sx={{
+              border: "2px solid black",
+              width: 1400,
+              height: 680,
+              backgroundColor: "#454545",
+              color: "white",
+              borderRadius: "10px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              alt={items.name}
+              image={items.img}
+              sx={{
+                border: "2px solid black",
+                margin: "20px",
+                width: 1360,
+                height: 450,
+                objectFit: "cover",
+                borderRadius: "10px",
+              }}
+            />
             <Stack
               direction="row"
               alignItems="center"
@@ -64,20 +104,37 @@ export const MaterialUI = () => {
               useFlexGap
             >
               <Stack direction="column" spacing={0.5} useFlexGap>
-                <Typography>{items.typo}</Typography>
-                <Stack direction="row" spacing={1} useFlexGap>
+                <Typography
+                  sx={{
+                    fontSize: "30px",
+                    marginLeft: "25px",
+                    marginBottom: "-20px",
+                  }}
+                >
+                  {items.typo}
+                </Typography>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  spacing={1}
+                  useFlexGap
+                >
                   <Chip
-                    size="small"
+                    size="medium"
                     label={activeStates[index] ? "Active" : "Inactive"}
                     color={activeStates[index] ? "success" : "default"}
                   />
-                  <Rating value={1} size="small" />
+                  <Rating value={1} size="medium" />
                 </Stack>
               </Stack>
-              <Switch
-                checked={activeStates[index]}
-                onChange={handleChange(index)}
-              />
+              <div style={{ marginLeft: "auto" }}>
+                <Switch
+                  color="secondary"
+                  size="big"
+                  checked={activeStates[index]}
+                  onChange={handleChange(index)}
+                />
+              </div>
             </Stack>
           </Card>
         ))}
